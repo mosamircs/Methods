c1ddb8b6360d386c5a33dfd98032ab4e454a0882
mosamircs <mosamircs@gmail.com>
2021-05-24 23:50:10 +0200

    out more than one output from method by out

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -14,13 +14,22 @@ namespace Methods
             p.EvenNumbers(30);//instance Method need instance object from class to invoke and for every object it can be called with different  prameters
             int FirstNumber = 20;
             int SecondNumber = 50;
-            Program.Add(FirstNumber,SecondNumber);//static Method called with class name not with object  
+            Program.Add(FirstNumber,SecondNumber);//static Method called with class name not with object
+            
+              
             int i = 0;
             Program p1 = new Program();
             //p1.SimpleFunction(i);//passinyg by value so it will not change because i and j referes to different memory locations
             p1.SimpleFunction(ref i);//passing by refernece making i and j refers to same location and when function change it will affect all
             Console.WriteLine(i);
 
+            //using out parameter to return more than one value from Method
+            int Sum;
+            int Mul;
+            int Sub;
+            int Div;
+            SimpleCalculator(10,0,out Sum, out Mul, out Sub, out Div);
+            Console.WriteLine("Sum is {0} and Sub is {1} and Mul is {2} and Div is {3}",Sum,Sub,Mul,Div);
            
         }
         //simple method
@@ -39,12 +48,25 @@ namespace Methods
                 }
             }
         }
+        public static void SimpleCalculator(int FirstNumber,int SecondNumber,out int Sum, out int Mul,out int Sub,out int Div)
+        {
+            Sum = FirstNumber + SecondNumber;
+            Sub = FirstNumber - SecondNumber;
+            Mul = FirstNumber * SecondNumber;
+            try
+            {
+                Div = FirstNumber / SecondNumber;
+            }catch(Exception e)
+            {
+                Div = 0;
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
         static void Add(int FirstNumber,int SecondNumber)
         {
             long Sum = FirstNumber + SecondNumber;
-            Console.WriteLine("Sum is {0}",Sum);
-            
-            
+            Console.WriteLine("Sum is {0}",Sum);          
         }
     }
 }
